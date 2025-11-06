5f60d3fedede5bb68f2c0de987d15f8ac835bdd4
Peter Akaliro <38269224+pin4eva@users.noreply.github.com>
2025-11-06 02:01:54 +0100

    Add Vercel configuration and refactor GraphQL API structure

--- api/index.ts
+++ api/index.ts
@@ -1,21 +1,18 @@
-let cachedApp: any;
+import {app} from '@getcronit/pylon'
 
-async function getPylonApp() {
-  if (!cachedApp) {
-    const module = await import('../.pylon/index.js')
-    cachedApp = module.default ?? module
-  }
-  return cachedApp
-}
-
-export default {
-  async fetch(request: Request) {
-    const app = await getPylonApp()
-
-    if (typeof app?.fetch !== 'function') {
-      throw new Error('Pylon app does not expose a fetch handler')
-    }
-
-    return app.fetch(request)
+export const graphql = {
+  Query: {
+    sum: (a: number, b: number) => a + b,
+    hello: () => 'Hello, world!'
+  },
+  Mutation: {
+    divide: (a: number, b: number) => a / b
   }
 }
+
+app.get('/health', (ctx, next) => {
+
+  return new Response('Welcome home')
+})
+
+export default app
