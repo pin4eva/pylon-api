730aeb72a40a5676b75c5a3040f6071e7963fd12
Peter Akaliro <38269224+pin4eva@users.noreply.github.com>
2025-11-06 02:42:56 +0100

    Add API endpoints for welcome message and status response

--- src/index.ts
+++ src/index.ts
@@ -9,4 +9,11 @@ export const graphql = {
   Mutation: {}
 }
 
+app.get("/api", () => {
+  return new Response("Welcome to the Pylon API")
+});
+
+app.get('/', () => {
+  return new Response('Ok', { status: 200 })
+})
 export default app
